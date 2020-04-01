22fb739bf9a51ab43826b27d51de125f385b05aa
CostelusE <62852858+CostelusE@users.noreply.github.com>
2020-04-01 12:22:29 +0300

    Add files via upload

---
+++ Problema 3/EvidentaStudenti/Program.cs
@@ -0,0 +1,53 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Student s = new Student("Ionescu");
+            s.SetNote("2 3 4,3 4 5");
+            s.AfisareStudNote();
+            Console.ReadKey();
+            bool ok=true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("S. Citire note de la tastatura ca un sir de caractere");
+                Console.WriteLine("T. Citire note de la tastatura(cate o nota pe sir)");
+                Console.WriteLine("C. Citire note din linia de comanda");
+                Console.WriteLine("A. Afisare note");
+                Console.WriteLine("X. Iesire");
+                Console.WriteLine("Introdu optiunea:");
+                
+                switch (Console.ReadLine())
+                {
+                    case "S":
+                       s.CitireTastatura();
+                        break;
+                    case "T":
+                        s.CitireTastaturaRand();
+                        break;
+                    case "C":
+                        
+
+                        s.CitireLinieComanda(args);
+                        break;
+                    case "A":
+                        s.Afisare();
+                        break;
+                    case "X":
+                        ok = false;
+                        break;
+                }
+                Console.ReadKey();
+                }
+                while(ok);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problema 3/EvidentaStudenti/Student.cs
@@ -0,0 +1,146 @@
+﻿using System;
+namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[][] note;
+        
+
+        // contructor implicit
+        public Student()
+        {
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+           
+        }
+
+        public void SetNote(string sirNote)
+        {
+            
+            // DE IMPLEMENTAT:
+            // extrage note din sir si adauga in matricea note
+
+            String[] prep = sirNote.Split(',');
+            note = new int[4][];
+            for (int l = 0; l < 4; l++)
+                note[l] = new int[15];
+
+            for (int i = 0; i < prep.Length; i++)
+            {
+                string[] prep2 = prep[i].Split(' ');
+                for (int j = 0; j < prep2.Length; j++)
+                    note[i][j] = Convert.ToInt32(prep2[j]);
+            }
+
+
+
+        }
+        public void AfisareStudNote()
+        {
+
+            
+            Console.WriteLine("Studentul {0} are urmatoarele note:\n",nume);
+            Afisare();
+
+            
+        }
+        public void CitireTastatura()
+
+        {
+            
+            String s1 = Console.ReadLine();            String [] prep = s1.Split(',');
+            note = new int[4][];
+            for (int l = 0; l < 4; l++)
+                note[l] = new int[15];
+            
+            for (int i = 0; i < prep.Length; i++)
+            {
+                string[] prep2 = prep[i].Split(' ');
+                for (int j = 0; j <prep2.Length ; j++)
+                    note[i][j] = Convert.ToInt32(prep2[j]);
+            }
+        }
+
+        public void CitireTastaturaRand()
+        {
+            int ok = 0;
+            note = new int[4][];
+            for (int l = 0; l < 4; l++)
+                note[l] = new int[15];
+            int i = 0;
+            int j = 0;
+            do
+            {
+                
+                string s1 = Console.ReadLine();
+                if (s1[0] == '.')
+                    break;
+                if (i < 4 || j < 15)
+                {
+                    if (s1[0] == ',')
+                    {
+                        i++;
+                        j = 0;
+
+                    }
+                    else
+                    {
+                        note[i][j] = Convert.ToInt32(s1);
+                        j++;
+                    }
+                }
+                
+            } while (true);
+           
+
+        }
+        public void CitireLinieComanda(string[] s1 )
+        {
+            int j, k;
+            note = new int[4][];
+            for (int l = 0; l < 4; l++)
+                note[l] = new int[15];
+            j = 0;
+            k = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+
+                if (s1[i] != ",")
+                {
+                    note[j][k] = Convert.ToInt32(s1[i]);
+                    k++;
+                }
+                else
+                {
+                    k = 0;
+                    j++;
+                }
+                
+            }
+
+
+
+        }
+    public void Afisare()
+        {
+            for (int i = 0; i <4 ; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                    if (note[i][j] != 0)
+                                            
+                        Console.Write("{0} ", note[i][j]);
+                if(note[i][0]!=0)    
+                Console.Write("\b,\n");
+               
+            }
+                   }
+
+
+    }
+}
